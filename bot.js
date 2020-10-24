75ab8e380957a9615d0272f5381ba4e50303d8ba
Nottinhaps <nottinhaps@gmail.com>
2020-10-24 23:07:01 +0800

    v2.2.3: Minor bug fixes
     - Music should play properly
     - Fixed welcome message

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ ytdl = require('ytdl-core');
 
 const botPrefix = "!";
 
-const version = "2.2.2"; // Version
+const version = "2.2.3"; // Version
 
 numRequests = 0;
 schedule.scheduleJob('/30 * * * * *', () => numRequests = 0);
